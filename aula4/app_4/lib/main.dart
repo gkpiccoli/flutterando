2f4602cecdc032c7c3329e3a9bbf2bbf9af30031
Gustavo Kureck Piccoli <gustavokpiccoli@gmail.com>
2022-11-29 16:53:06 -0300

    update

--- aula4/app_4/lib/main.dart
+++ aula4/app_4/lib/main.dart
@@ -1,7 +1,43 @@
 import 'package:flutter/material.dart';
 
-import 'home_page.dart';
+void main() => runApp(const MyApp());
 
-void main() {
-  runApp(const PaginaInicial());
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  static const String _title = 'Flutter - Testes';
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: _title,
+      home: PaginaInicial(),
+    );
+  }
+}
+
+class PaginaInicial extends StatefulWidget {
+  const PaginaInicial({super.key});
+
+  @override
+  State<PaginaInicial> createState() => _PaginaInicialState();
+}
+
+class _PaginaInicialState extends State<PaginaInicial> {
+  int _count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter'),
+      ),
+      body: Center(child: Text('Você pressionou o botão $_count vezes.')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => setState(() => _count++),
+        tooltip: 'Adicionar Item',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
 }
